a6c7aeb8132c8c3952c0e9a931f61444dd9ac103
Frank0978 <zbneavides@gmail.com>
2024-03-07 21:23:15 +0100

    Create app.blade.php

---
+++ views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pagina Web</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="public/assets/css/stylesheet.css">
+    </head>
+    <body>
+                @yield('content')
+    </body>
+</html> 
